4539048c23fa8fe66d79ccd36ae246e13a99470c
Igor Bedesqui <igorbedesqui@gmail.com>
2023-04-04 20:20:01 +0100

    this finally fucking worked OMG I love monorepos

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,7 +1,12 @@
 import { Button } from "ui";
-import { lazyThing } from "lazy-things";
+import { lazyThing } from "@bdsqqq/lazytm";
+import { useEffect } from "react";
 
 export default function Web() {
+  useEffect(() => {
+    lazyThing();
+  }, []);
+
   return (
     <div>
       <h1>Web</h1>
