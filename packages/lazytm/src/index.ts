521e00fcdad33a3db213eb945425d91426c3c4bc
Igor Bedesqui <igorbedesqui@gmail.com>
2023-04-05 23:51:45 +0100

    remove internals exposure

--- packages/lazytm/src/index.ts
+++ packages/lazytm/src/index.ts
@@ -1,7 +1,3 @@
-import { DANGEROUS_INTERNALS_DONT_USE_OR_YOU_WILL_BE_FIRED } from "./internals";
 import { makeLazySplitIterator } from "./lazySplit";
 
-export {
-  makeLazySplitIterator,
-  DANGEROUS_INTERNALS_DONT_USE_OR_YOU_WILL_BE_FIRED,
-};
+export { makeLazySplitIterator };
--- packages/lazytm/src/internals.ts
+++
@@ -1,3 +0,0 @@
-export const DANGEROUS_INTERNALS_DONT_USE_OR_YOU_WILL_BE_FIRED = {
-  version: "1",
-};
